b3e5a99e46f00b33ff883ed8eec6469ad7cbcc2d
Hassan Abid <hassanabidpk89@gmail.com>
2016-07-26 19:35:14 +0900

    Step-4(c) Make migrations and add test case

--- menu/tests.py
+++ menu/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Cupcake
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your tests here.
+class MenuTestCase(TestCase):
+    def setUp(self):
+        u = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        Cupcake.objects.create(name="chocolate",rating=5, writer=u,price="20")
+        Cupcake.objects.create(name="earl grey",rating=4, writer=u,price="10")
+
+    def test_cupcake_has_name(self):
+        """Menu Test
+            Fields tested : name,rating,writer
+        """
+        cupcake1 = Cupcake.objects.get(name="chocolate")
+        cupcake2 = Cupcake.objects.get(name="earl grey")
+        self.assertEqual(cupcake1.name, 'chocolate')
+        self.assertEqual(cupcake1.rating, 5)
+        self.assertEqual(cupcake2.price, '10')
+        self.assertEqual(cupcake1.price, '20')
