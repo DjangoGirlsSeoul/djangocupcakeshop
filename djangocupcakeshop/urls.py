fd4df1771bc7d6f836772380c17be6e55bcf1c54
Hassan Abid <hassanabidpk89@gmail.com>
2016-07-26 23:18:33 +0900

    Step-6(a,b) Add django-urls

--- djangocupcakeshop/urls.py
+++ djangocupcakeshop/urls.py
@@ -21,4 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('menu.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
