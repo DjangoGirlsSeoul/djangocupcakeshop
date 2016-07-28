9082204bb583ed398c0d5bd7f0a651e94a5b3890
Hassan Abid <hassanabidpk89@gmail.com>
2016-07-28 10:40:28 +0900

    add media settings

--- djangocupcakeshop/urls.py
+++ djangocupcakeshop/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
