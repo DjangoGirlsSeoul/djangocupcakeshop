9cd183852959cb1e9e43bf57b85e43091cacc13f
Hassan Abid <hassanabidpk89@gmail.com>
2016-07-30 01:11:37 +0900

    Step-11(c) Add view function for form

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from .models import Cupcake
+from .forms import CupcakeForm
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
 
 def cupcake_list(request):
     cakes = Cupcake.objects.all().order_by('-createdAt')
@@ -10,3 +14,21 @@ def cupcake_detail(request,pk):
     cake = get_object_or_404(Cupcake,pk=pk)
     context = {"cake": cake}
     return render(request,"menu/detail.html",context)
+
+@login_required
+def cupcake_new(request):
+    if request.method == "POST":
+        print("here - ")
+        form = CupcakeForm(request.POST,request.FILES)
+        if form.is_valid():
+            print("here - 2")
+            cake = form.save(commit=False)
+            cake.createdAt = timezone.now()
+            cake.writer = request.user
+            cake.save()
+            print("here ---")
+            return redirect('cupcake_detail',pk=cake.pk)
+    else:
+        form = CupcakeForm()
+    context = {'form':form}
+    return render(request,"menu/cupcake_new.html",context)
