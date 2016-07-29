4bbd939ea9cbd32add73299f5bdc94d623ea1807
Hassan Abid <hassanabidpk89@gmail.com>
2016-07-30 00:47:16 +0900

    Step-11(a) Create ModelForm for Cupcake

--- menu/urls.py
+++ menu/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.cupcake_list,name="cupcake_list"),
-    url(r'^cupcake/(?P<pk>\d+)/$',views.cupcake_detail,name="cupcake_detail")
+    url(r'^cupcake/(?P<pk>\d+)/$',views.cupcake_detail,name="cupcake_detail"),
+    url(r'^cupcake/new/$', views.cupcake_new, name='cupcake_new'),
 ]
