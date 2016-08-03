99afdedd147297171a82844639430f56d4e463b5
Hassan Abid <hassanabidpk89@gmail.com>
2016-08-03 10:35:06 +0900

    test travis and coveralls

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Cupcake(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.CharField(max_length=20)
+    rating = models.FloatField()
+    image = models.ImageField(upload_to='images/cakes')
+    writer = models.ForeignKey(User)
+    createdAt = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
